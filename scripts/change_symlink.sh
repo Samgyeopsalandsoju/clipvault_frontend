2de49eda4e28aefe1e7a41d5336b990e1c93e30d
Jaden <hyunbin.kim.3376@gmail.com>
2025-02-03 04:45:24 +0900

    Test: upload to S3 and codeDeploy test v3

--- scripts/change_symlink.sh
+++ scripts/change_symlink.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-
-DEPLOY_DIR=~/frontend/releases/$(date +%Y%m%d%H%M%S)
-CURRENT_LINK=~/frontend/current
+DEPLOY_DIR=/home/ec2-user/frontend/releases/$(date +%Y%m%d%H%M%S)
+CURRENT_LINK=/home/ec2-user/frontend/current
 
 # 새로운 배포 폴더로 이동
 mkdir -p "$DEPLOY_DIR"
-mv ~/frontend/releases/new-release/* "$DEPLOY_DIR"
+mv /home/ec2-user/frontend/releases/new-release/* "$DEPLOY_DIR"
 
 # 심볼릭 링크 변경
 ln -nfs "$DEPLOY_DIR" "$CURRENT_LINK"
--- scripts/restart_service.sh
+++ scripts/restart_service.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-cd ~/frontend/current
+cd /home/ec2-user/frontend/current
 
-if pm2 list | grep -q "clipvault"; then
-    pm2 restart clipvault
+# pm2 명령어도 전체 경로로 변경합니다
+if /home/ec2-user/.local/share/pnpm/pm2 list | grep -q "clipvault"; then
+    /home/ec2-user/.local/share/pnpm/pm2 restart clipvault
 else
-    pm2 start "pnpm start" --name clipvault
+    /home/ec2-user/.local/share/pnpm/pm2 start "pnpm start" --name clipvault
 fi
 
 echo "Next.js server restarted!"
