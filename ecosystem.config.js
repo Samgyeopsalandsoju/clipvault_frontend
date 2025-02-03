084f0f7983eeb05501a21a051e2a2147d2769636
Jaden <hyunbin.kim.3376@gmail.com>
2025-02-03 17:04:01 +0900

    Test: upload to S3 and codeDeploy test v13

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,10 +2,10 @@ module.exports = {
   apps: [
     {
       name: 'clipvault',
-      script: '.next/standalone/server.js', // standalone 모드 사용
-      instances: 1, // 단일 인스턴스
-      exec_mode: 'fork', // fork 모드 사용
-      watch: false, // 리소스 절약을 위해 watch 비활성화
+      script: '.next/standalone/server.js',
+      instances: 1,
+      exec_mode: 'fork',
+      watch: false,
       env: {
         PORT: 3000,
         NODE_ENV: 'production',
@@ -14,7 +14,7 @@ module.exports = {
       error_file: '/home/ec2-user/frontend/logs/error.log',
       out_file: '/home/ec2-user/frontend/logs/out.log',
       log_date_format: 'YYYY-MM-DD HH:mm Z',
-      max_memory_restart: '512M', // t2.micro는 1GB 메모리이므로 512MB로 제한
+      max_memory_restart: '512M',
       autorestart: true,
       restart_delay: 4000,
       wait_ready: true,
