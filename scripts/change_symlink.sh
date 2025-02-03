ee02210699a1a8421fcc893679c72d4c555bb604
Jaden <hyunbin.kim.3376@gmail.com>
2025-02-04 02:41:46 +0900

    Test: upload to S3 node_module v4

--- scripts/change_symlink.sh
+++ scripts/change_symlink.sh
@@ -11,4 +11,9 @@ mv /home/ec2-user/frontend/releases/new-release/.* "$DEPLOY_DIR/" 2>/dev/null ||
 # 심볼릭 링크 변경
 ln -nfs "$DEPLOY_DIR" "$CURRENT_LINK"
 
+
+# 정적 파일 권한 설정
+# $DEPLOY_DIR을 사용하여 정확한 새 배포 경로의 파일 권한을 설정합니다
+sudo chmod -R 755 "$DEPLOY_DIR/.next"
+
 echo "Deployment completed. Current version: $DEPLOY_DIR"
