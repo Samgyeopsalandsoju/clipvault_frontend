4f514fcd2162e6b84ee44a74dfae60adebb65936
Jaden <hyunbin.kim.3376@gmail.com>
2025-02-24 23:56:16 +0900

    test: deploy

--- src/utils/link.ts
+++ src/utils/link.ts
@@ -32,7 +32,6 @@ export const checkProtocol = (url: string): URLValidationResult => {
 
 export const filterXSSInUrl = (url: string): URLValidationResult => {
   const decodedUrl = decodeURIComponent(url);
-  // const xssPattern = /^(?!.*(?:<[^>]*>|javascript\s*:|data\s*:|vbscript\s*:|on\w+\s*=|eval\s*\(|expression\s*\()).*$/i;
   // XSS 위험 패턴 감지
   const xssPattern = /^(?!.*(?:<[^>]*>|javascript\s*:|data\s*:|vbscript\s*:|on\w+\s*=|eval\s*\(|expression\s*\()).*$/i;
 
