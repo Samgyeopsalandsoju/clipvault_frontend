e663c69f800480134877058e45be2663b8285ca9
Jaden <hyunbin.kim.3376@gmail.com>
2025-02-25 01:17:50 +0900

    test: deploy

--- next.config.ts
+++ next.config.ts
@@ -18,6 +18,23 @@ const nextConfig: NextConfig = {
       },
     ];
   },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false, // 서버 전용 모듈 제거
+        path: false,
+      };
+    }
+
+    // Webpack 최적화 설정 추가
+    config.optimization = {
+      ...config.optimization,
+      usedExports: true, // 사용된 export만 번들 포함
+      sideEffects: false, // 불필요한 코드 제거 방지
+    };
+
+    return config;
+  },
 };
 
 export default nextConfig;
