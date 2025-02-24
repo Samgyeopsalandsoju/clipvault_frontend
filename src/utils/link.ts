db0af3bb353536d8e7352d0f0d80f4bd80c5c224
Jaden <hyunbin.kim.3376@gmail.com>
2025-02-24 23:43:58 +0900

    fix: url xss filter

--- src/utils/link.ts
+++ src/utils/link.ts
@@ -34,7 +34,7 @@ export const filterXSSInUrl = (url: string): URLValidationResult => {
   const decodedUrl = decodeURIComponent(url);
   // const xssPattern = /^(?!.*(?:<[^>]*>|javascript\s*:|data\s*:|vbscript\s*:|on\w+\s*=|eval\s*\(|expression\s*\()).*$/i;
   // XSS 위험 패턴 감지
-  const xssPattern = /<[^>]*>|javascript\s*:|data\s*:|vbscript\s*:|on\w+\s*=|eval\s*\(|expression\s*\(/i;
+  const xssPattern = /^(?!.*(?:<[^>]*>|javascript\s*:|data\s*:|vbscript\s*:|on\w+\s*=|eval\s*\(|expression\s*\()).*$/i;
 
   console.log('url', url);
   console.log('decodedUrl', decodedUrl);
