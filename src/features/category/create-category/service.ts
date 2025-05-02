fc4b9f569028f6577d0019c722df872e5a77dbce
Jaden <hyunbin.kim.3376@gmail.com>
2025-05-03 02:35:45 +0900

    feat: 카테고리 추가 서비스 설정

--- src/features/category/create-category/service.ts
+++ src/features/category/create-category/service.ts
@@ -4,6 +4,7 @@ import { ICategoryForm } from './model/type';
 
 // 카테고리 생성
 export const postCategory = async (category: ICategoryForm) => {
-  const res = await apiClient.post<APIResponse<string>>('/category/create', category);
+  const jsonData = { ...category, id: '' };
+  const res = await apiClient.post<APIResponse<string>>('/category/create', jsonData);
   return res.data.body;
 };
