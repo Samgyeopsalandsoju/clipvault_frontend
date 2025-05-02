7ad44ef6b3dfbfe67a52919fc3a5e5c96bb9a259
Jaden <hyunbin.kim.3376@gmail.com>
2025-05-03 02:36:10 +0900

    feat: 카테고리 추가 UI 설정

--- src/features/category/create-category/ui/CreateCategory.tsx
+++ src/features/category/create-category/ui/CreateCategory.tsx
@@ -17,48 +17,69 @@ export const CreateCategory = ({ isOpen, onClose }: { isOpen: boolean; onClose: 
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<ICategoryForm>();
+    setError,
+    setValue,
+    clearErrors,
+    reset,
+  } = useForm<ICategoryForm>({ mode: 'onChange' });
   const [color, setColor] = useState<string>('');
-  const { postCategory } = useCreateCategory();
+  const { postCategory, isLoading } = useCreateCategory();
+
+  const selectColor = (newColor: string) => {
+    // newColor가 설정되어있다면 set 하고 에러 클리어
+    if (newColor.length > 0) {
+      setColor(newColor);
+      setValue('color', newColor);
+      clearErrors('color');
+    }
+  };
 
   const onSubmit = (data: ICategoryForm) => {
-    postCategory(data);
+    if (color.length === 0) {
+      setError('color', { message: '색상을 선택해 주세요.' });
+      return;
+    }
+    postCategory(data, {
+      onSuccess: () => {
+        onClose();
+        reset();
+      },
+    });
   };
 
   // 닫기 핸들러
   const handleClose = () => {
     onClose();
   };
-  console.log(color);
   return (
     <Drawer open={isOpen} onOpenChange={handleClose}>
-      <DrawerContent
-        className={clsx(
-          'flex justify-center items-center m-auto w-full',
-          'md:w-[600px]',
-          'lg:w-[800px]'
-        )}
-      >
+      <DrawerContent className={clsx('flex justify-center items-center m-auto w-full', 'md:w-[600px]', 'lg:w-[800px]')}>
         <DrawerHeader>
           <DrawerTitle>Create category</DrawerTitle>
         </DrawerHeader>
-        <form
-          className="flex flex-col gap-6 w-full px-4 lg:w-[40vw] py-4"
-          onSubmit={handleSubmit(onSubmit)}
-        >
+        <form className="flex flex-col gap-6 w-full px-4 lg:w-[40vw] py-4" onSubmit={handleSubmit(onSubmit)}>
           {/** 카테고리 이름*/}
           <div className="grid gap-2 ">
             <Label htmlFor="title">카테고리 이름</Label>
-            <Input id="title" type="text" placeholder="카테고리" {...register('categoryName')} />
+            <Input
+              id="categoryName"
+              type="text"
+              placeholder="카테고리"
+              {...register('categoryName', { required: '카테고리 이름을 입력해주세요.' })}
+            />
+            <span className="text-sm text-red-500 h-3">{errors?.categoryName?.message || ' '}</span>
           </div>
 
           {/** 색상 선택 링크*/}
           <div className="grid gap-2 ">
             <Label htmlFor="title">카테고리 색상</Label>
-            <ColorPicker color={color} setColor={setColor} />
+            <ColorPicker color={color} setColor={selectColor} />{' '}
+            <span className="text-sm text-red-500 h-3">{errors?.color?.message || ' '}</span>
           </div>
 
-          <Button type="submit">카테고리 생성</Button>
+          <Button type="submit" disabled={isLoading}>
+            카테고리 생성
+          </Button>
         </form>
       </DrawerContent>
     </Drawer>
