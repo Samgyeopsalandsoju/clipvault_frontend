0f77b0cd0d3580230e049c821e7d70dbfca1ccc6
Jaden <hyunbin.kim.3376@gmail.com>
2025-05-03 01:37:57 +0900

    feat: api router url 직관적으로 수정 user-get -> user-clip-get & public-get -> public-clip-get

--- src/features/clips/community-clips/service.ts
+++ src/features/clips/community-clips/service.ts
@@ -4,6 +4,6 @@ import { IHomeClip } from '@/shared/types/clip';
 
 // 홈 클립 리스트 서비스 레이어
 export const getCommunityClips = async () => {
-  const res = await apiClient.get<APIResponse<IHomeClip[]>>('/clip/public-get');
+  const res = await apiClient.get<APIResponse<IHomeClip[]>>('/clip/public-clip-get');
   return res.data.body;
 };
--- src/features/clips/user-clips/service.ts
+++ src/features/clips/user-clips/service.ts
@@ -4,6 +4,6 @@ import { IUserClip } from '@/shared/types/clip';
 
 // 유저 클립 리스트 서비스 레이어
 export const getUserClips = async () => {
-  const res = await apiClient<APIResponse<IUserClip[]>>('/clip/user-get');
+  const res = await apiClient<APIResponse<IUserClip[]>>('/clip/user-clip-get');
   return res.data.body;
 };
