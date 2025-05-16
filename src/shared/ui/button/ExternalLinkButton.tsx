9d44be32348559323046f27a6c64a33ef214fcbf
Jaden <hyunbin.kim.3376@gmail.com>
2025-05-16 20:26:29 +0900

    feat: shared - 새창 열기 버튼 구현

---
+++ src/shared/ui/button/ExternalLinkButton.tsx
@@ -0,0 +1,13 @@
+import { openInNewTab } from '@/shared/core/utils';
+import { ExternalLink } from 'lucide-react';
+
+export const ExternalLinkButton = ({ link }: { link: string }) => {
+  return (
+    <div
+      className="p-1.5 rounded-full text-gray-400 hover:text-gray-700 hover:bg-gray-100 transition-colors cursor-pointer"
+      onClick={(e) => openInNewTab(link)}
+    >
+      <ExternalLink size={16} />
+    </div>
+  );
+};
