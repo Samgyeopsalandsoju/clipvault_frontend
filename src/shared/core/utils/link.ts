be1f48dcf542e6f84da7f9407213cfe7decf61bc
Jaden <hyunbin.kim.3376@gmail.com>
2025-05-19 21:16:16 +0900

    feat: 이벤트 버블링으로 인해 클릭이 중첩되는 문제 해결

--- src/shared/core/utils/link.ts
+++ src/shared/core/utils/link.ts
@@ -6,7 +6,8 @@ export const copyLink = (e: React.MouseEvent, url: string, onSuccess: () => void
 
 // TODO: 실제 Url이 어떤 형식으로 사용되는지 확인 후
 // 다시 체크
-export const openInNewTab = (url: string) => {
+export const openInNewTab = (e: React.MouseEvent, url: string) => {
+  e.stopPropagation();
   if (url.startsWith('https://')) {
     window.open(url, '_blank', 'noopener,noreferrer');
   } else {
--- src/shared/ui/button/ExternalLinkButton.tsx
+++ src/shared/ui/button/ExternalLinkButton.tsx
@@ -5,7 +5,7 @@ export const ExternalLinkButton = ({ link }: { link: string }) => {
   return (
     <div
       className="p-1.5 rounded-full text-gray-400 hover:text-gray-700 hover:bg-gray-100 transition-colors cursor-pointer"
-      onClick={(e) => openInNewTab(link)}
+      onClick={(e) => openInNewTab(e, link)}
     >
       <ExternalLink size={16} />
     </div>
